78151284b09cc2b8252f3a6429e5f73e36c7768a
Mentor Reka <reka.mentor@gmail.com>
2018-10-27 16:32:58 +0200

    Init country map with data from API

--- swiss-devs-frontend/assets/js/switzerland-chart.js
+++ swiss-devs-frontend/assets/js/switzerland-chart.js
@@ -1,10 +1,69 @@
-// Initialize the Switzerland MAP with the count numbers
+// Initialize the Switzerland MAP with the count numbers stored in DataBase
 function initMap() {
     let API_ROOT = "http://localhost:3000";
-    let ENDPOINT_USERS_COUNT_PER_CANTON = "/users/canton/Vaud/count";
+    let ENDPOINT_USERS_COUNT_PER_CANTON = "/users/canton/count";
 
-    $.get( API_ROOT + ENDPOINT_USERS_COUNT_PER_CANTON, function( data ) {
-        console.log(data);
+    var custom_data = [];
+    $.get( API_ROOT + ENDPOINT_USERS_COUNT_PER_CANTON, function( payload ) {
+        // Create the dataset from payload
+        for (var _obj in payload) {
+            if (_obj == "undefined") {
+                continue;
+            }
+            custom_data.push(new Array(_obj, payload[_obj]));
+        }
+        
+        // Create the chart
+        Highcharts.mapChart('chart-switzerland', {
+            chart: {
+                map: 'countries/ch/ch-all',
+                backgroundColor: '#EEEEEE'
+            },
+            plotOptions:{
+                series:{
+                    cursor: 'pointer',
+                    point:{
+                        events:{
+                            click: function(){
+                                console.log(this.name);
+                            }
+                        }
+                    }
+                }
+            },
+        
+            title: {
+                text: 'Switzerland Cantons'
+            },
+        
+            subtitle: {
+                text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/ch/ch-all.js">Switzerland</a>'
+            },
+        
+            mapNavigation: {
+                enabled: false,
+            },
+        
+            colorAxis: {
+                min: 0
+            },
+        
+            series: [{
+                data: custom_data,
+                name: 'Github users',
+                states: {
+                    hover: {
+                        color: '#da291c'
+                    }
+                },
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                },
+                
+            }]
+        });
+
     });
 }
 
@@ -12,6 +71,8 @@ function initMap() {
 function loadUsers() {
 
 }
+
+initMap();
 
 
 // Prepare demo data
@@ -46,82 +107,4 @@ For a better understanding of Highcharts code
 "ch-zg": "Zu"
 "ch-zh": "Zurich"
 */
-var data = [
-    ['ch-fr', 0],
-    ['ch-ne', 1],
-    ['ch-ag', 2],
-    ['ch-lu', 3],
-    ['ch-7', 10], // Nidwald
-    ['ch-vs', 5],
-    ['ch-sg', 6],
-    ['ch-ar', 7],
-    ['ch-ti', 8],
-    ['ch-gl', 9],
-    ['ch-gr', 10],
-    ['ch-sz', 11],
-    ['ch-tg', 12],
-    ['ch-sh', 13],
-    ['ch-ur', 14],
-    ['ch-zh', 15],
-    ['ch-zg', 16],
-    ['ch-vd', 17],
-    ['ch-3306', 10], // Basel-Landschaft
-    ['ch-be', 19],
-    ['ch-bs', 20],
-    ['ch-so', 21],
-    ['ch-nw', 22],
-    ['ch-ai', 23],
-    ['ch-ge', 24],
-    ['ch-ju', 25]
-];
 
-// Create the chart
-Highcharts.mapChart('chart-switzerland', {
-    chart: {
-        map: 'countries/ch/ch-all',
-        backgroundColor: '#EEEEEE'
-    },
-    plotOptions:{
-        series:{
-            cursor: 'pointer',
-            point:{
-                events:{
-                    click: function(){
-                        console.log(this.name);
-                    }
-                }
-            }
-        }
-    },
-
-    title: {
-        text: 'Switzerland Cantons'
-    },
-
-    subtitle: {
-        text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/ch/ch-all.js">Switzerland</a>'
-    },
-
-    mapNavigation: {
-        enabled: false,
-    },
-
-    colorAxis: {
-        min: 0
-    },
-
-    series: [{
-        data: data,
-        name: 'Github users',
-        states: {
-            hover: {
-                color: '#da291c'
-            }
-        },
-        dataLabels: {
-            enabled: true,
-            format: '{point.name}'
-        },
-        
-    }]
-});
